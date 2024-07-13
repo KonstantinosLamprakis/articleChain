a29c08cf0d688b8168e4f16589965140e839aea3
Bankhardt Máté <bankhardtmate@Bankhardts-MacBook-Air.local>
2024-07-13 12:55:05 +0200

    make sketch for read

---
+++ write_and_read/read_data.js
@@ -0,0 +1,21 @@
+import { web3auth, contractABI, contractAddress } from "./init.js";
+
+function isJournalist(journalistAddress) {
+
+}
+
+function getArticle(articleId) {
+
+}
+
+function getCredibility(journalistAddress) {
+
+}
+
+function searchArticle(articleId) {
+
+}
+
+function provideArticleToJournalist(articleId, journalistAddress) {
+
+}
