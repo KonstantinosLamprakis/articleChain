842631bac739d02533ae22cd4ce7701e519394cf
Bankhardt Máté <bankhardtmate@Bankhardts-MacBook-Air.local>
2024-07-13 13:20:29 +0200

    write sketch

--- write_and_read/write_data.js
+++ write_and_read/write_data.js
@@ -1,1 +1,22 @@
 import { web3auth, contractABI, contractAddress } from "./init.js";
+
+
+export async function contract_op() {
+
+}
+
+export async function addJournalist(journalistAddress) {
+
+}
+
+export async function createArticle(filecoinCID) {
+
+}
+
+export async function evaluateArticle(articleId, approve, comment) {
+
+}
+
+export async function voteArticle(articleId, vote) {
+
+}
